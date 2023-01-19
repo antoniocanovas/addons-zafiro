9c6aba82da156cc2e94cc8c83cfd25561358a35b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-19 03:27:08 +0100

    zafiropet v0

--- pet_product/models/res_partner.py
+++ pet_product/models/res_partner.py
@@ -10,3 +10,11 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     type_ids = fields.Many2many('partner.type', string="Collaborations")
+
+    @api.depends('type_ids')
+    def get_type_char(self):
+        typechar = ""
+        for li in self.type_ids:
+            typechar += li.code
+        self.type_char = typechar
+    type_char = fields.Char('Collaborations', store=True, compute='get_type_char')
