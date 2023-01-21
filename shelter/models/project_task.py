07567528baba70e0e7fa55feb96fba3fe64a2b83
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-21 11:12:35 +0100

    zafiropet v0

--- shelter/models/project_task.py
+++ shelter/models/project_task.py
@@ -9,7 +9,8 @@ from odoo import api, fields, models, _
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    product_id      = fields.Many2one('product.template', string='Pet')
+    product_id      = fields.Many2one('product.product', string='Pet')
     home_id         = fields.Many2one('res.partner', string='Home', domain="[('type_char','ilike','home')]")
     veterinary_id   = fields.Many2one('res.partner', string='Veterinary', domain="[('type_char','ilike','veterinary')]")
     volunteer_id    = fields.Many2one('res.partner', string='Volunteer', domain="[('type_char','ilike','volunteer')]")
+    product_image   = fields.Binary('Image', related='product_id.image_1920')
