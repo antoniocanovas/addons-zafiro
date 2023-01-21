11ec0854bea3873176c0a6d80b779a30be6526f2
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-21 02:18:36 +0100

    zafiropet v0

--- shelter/models/project_task.py
+++ shelter/models/project_task.py
@@ -11,5 +11,5 @@ class ProjectTask(models.Model):
 
     product_id      = fields.Many2one('product.template', string='Pet')
     home_id         = fields.Many2one('res.partner', string='Home', domain="[('type_char','ilike','home')]")
-    veterinary_id   = fields.Many2one('res.partner', string='Home', domain="[('type_char','ilike','veterinary')]")
-    volunteer_id    = fields.Many2one('res.partner', string='Home', domain="[('type_char','ilike','volunteer')]")
+    veterinary_id   = fields.Many2one('res.partner', string='Veterinary', domain="[('type_char','ilike','veterinary')]")
+    volunteer_id    = fields.Many2one('res.partner', string='Volunteer', domain="[('type_char','ilike','volunteer')]")
