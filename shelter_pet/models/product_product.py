dd837b755cb3175d5e081ced1908b5b11d72fcdb
pedroguirao <pedro@serincloud.com>
2023-01-24 10:29:44 +0100

    Kanban stages

--- shelter_pet/models/product_product.py
+++ shelter_pet/models/product_product.py
@@ -9,6 +9,18 @@ from odoo import api, fields, models, _
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    stage_id    = fields.Many2one('product.stage', string="Stage", default="New")
+
     task_ids = fields.One2many('project.task', 'product_id', string='Tasks')
     pet_ok   = fields.Boolean('Pet', default=True)
+
+    def _default_stage(self):
+        return self.env['product.stage'].search([('name', '=', 'New')], limit=1).id
+
+    @api.model
+    def _read_group_stage_ids(self, stages, domain, order):
+        stage_ids = self.env['product.stage'].search([])
+        return stage_ids
+
+    stage_id = fields.Many2one('product.stage', string='Stage', store=True, readonly=False, ondelete='restrict',
+                               tracking=True, index=True, group_expand='_read_group_stage_ids', copy=False,
+                               default=_default_stage)
