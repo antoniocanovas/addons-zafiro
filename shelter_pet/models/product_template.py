4f90f1aa69c1befc4274f1e3f195b9109ebfdf25
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-25 02:23:46 +0100

    zafiropet v0

--- shelter_pet/models/product_template.py
+++ shelter_pet/models/product_template.py
@@ -14,6 +14,7 @@ class ProductTemplate(models.Model):
     responsible_id = fields.Many2one('res.users', string='Responsible (staff): ', store=True)
     veterinary_id = fields.Many2one('res.partner', string='Veterinary: ', store=True)
     home_id = fields.Many2one('res.partner', string='Home: ', store=True)
+    volunteer_id = fields.Many2one('res.partner', string='Volunteer: ', store=True)
 
 #    def _default_stage(self):
 #        return self.env['product.stage'].search([('name', '=', 'New')], limit=1).id
