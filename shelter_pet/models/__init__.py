178ee691019c6815a6d15087626132e78de81c7c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-25 02:20:09 +0100

    zafiropet v0

--- shelter_pet/models/__init__.py
+++ shelter_pet/models/__init__.py
@@ -3,7 +3,7 @@
 #    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 #    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
 ##############################################################################
-from . import product_stage
+#from . import product_stage
 #from . import product_template
 #from . import res_partner
 #from . import partner_type
