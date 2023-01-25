88c11b737626cb1344ea425f7c97111135618f9e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-25 10:17:42 +0100

    zafiropet v0

--- shelter_crm/models/crm_lead.py
+++ shelter_crm/models/crm_lead.py
@@ -10,11 +10,3 @@ class CrmLead(models.Model):
         column1='lead_id',
         column2='product_id',
     )
-
-    @api.depends('create_date')
-    def get_lead_self(self):
-        for record in self:
-            lead = self.env['crm.lead'].search([('id', '=', record.id)])
-            record.self = lead.id
-    self = fields.Many2one('crm.lead', string="Self", store=True, compute="get_lead_self")
-
