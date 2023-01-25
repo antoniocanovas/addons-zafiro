710af5403cf7166eba579a964a5af24f4415c3e6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-25 16:39:52 +0100

    pet website

---
+++ shelter_website/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'CRM Website',
+    'version': '16.0.1.0.0',
+    'category': '',
+    'description': u"""
+Website and eCommerce devs
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'shelter_pet',
+        'website_sale',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+}
