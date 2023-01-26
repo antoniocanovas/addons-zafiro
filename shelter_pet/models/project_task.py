ab6de52e93622005dfbcf9c136006cfc21c5e2dd
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-26 03:02:29 +0100

    pet 2

--- shelter_pet/models/project_task.py
+++ shelter_pet/models/project_task.py
@@ -11,7 +11,7 @@ class ProjectTask(models.Model):
 
     product_id      = fields.Many2one('product.product', string='Pet', store=True)
     product_tmpl_id = fields.Many2one('product.template', string='Pet template', related='product_id.product_tmpl_id', store=True)
-    home_id         = fields.Many2one('res.partner', string='Home', domain="[('type_char','ilike','home')]")
+    home_id         = fields.Many2one('stock.location', string='Home', related='product_id.location_id', readonly=False)
     veterinary_id   = fields.Many2one('res.partner', string='Veterinary', domain="[('type_char','ilike','veterinary')]")
     volunteer_id    = fields.Many2one('res.partner', string='Volunteer', domain="[('type_char','ilike','volunteer')]")
     product_image   = fields.Binary('Image', related='product_id.image_1920', store=False)
