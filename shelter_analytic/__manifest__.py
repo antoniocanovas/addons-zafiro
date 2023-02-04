0d2ba75ed61d3dca41da8044d1f16cbd3c4cdb86
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-04 18:01:15 +0100

    shelter_analytic v0

---
+++ shelter_analytic/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'CRM Website',
+    'version': '16.0.1.0.0',
+    'category': '',
+    'description': u"""
+Shelter pet analytic
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'shelter_pet',
+        'product_analytic',
+    ],
+    'data': [
+#        'views/product_views.xml',
+    ],
+    'installable': True,
+}
