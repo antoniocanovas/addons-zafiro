d2889759ee40ec325a802649ee930e40b7ec87c3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-05 10:16:10 +0100

    shelter_analytic v0

--- shelter_analytic/models/res_company.py
+++ shelter_analytic/models/res_company.py
@@ -4,4 +4,4 @@ from odoo import _, api, fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    shelter_analytic_plan = fields.Many2one('account.analytic.plan','Shelter Analytic Plan', store=True)
+    shelter_analytic_plan = fields.Many2one('account.analytic.plan','Shelter Analytic Plan', store=True, default=1)
