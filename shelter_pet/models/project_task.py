e108f0416c81a7d4d05813a96eea1003e11c952a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-18 00:13:16 +0100

    shelter project type

--- shelter_pet/models/project_task.py
+++ shelter_pet/models/project_task.py
@@ -16,3 +16,12 @@ class ProjectTask(models.Model):
     veterinary_id   = fields.Many2one('res.partner', string='Veterinary', store=True)
     volunteer_id    = fields.Many2one('res.partner', string='Volunteer', store=True)
     product_image   = fields.Binary('Image', related='product_id.image_1920', store=False)
+
+    @api.depends('project_id.type_id')
+    def _get_project_type_char(self):
+        type_char = ""
+        if self.project_id.type_id.id == self.company_id.veterinary_type_id.id: type_char = 'veterinary'
+        if self.project_id.type_id.id == self.company_id.network_type_id.id:    type_char = 'network'
+        if self.project_id.type_id.id == self.company_id.management_type_id.id: type_char = 'management'
+        self.project_type = type_char
+    project_type    = fields.Char('Project type', store=True, compute='_get_project_type_char')
