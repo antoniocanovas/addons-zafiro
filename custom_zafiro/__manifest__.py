7a949247cef39b6a95e505f2a52c201e4a3f41e8
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-01 01:12:18 +0100

    shelter project calendar

---
+++ custom_zafiro/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Custom Zafiro',
+    'version': '16.0.1.0.0',
+    'category': '',
+    'description': u"""
+Zafiro particular customizations
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'shelter_website',
+        'hide_cart',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+}
